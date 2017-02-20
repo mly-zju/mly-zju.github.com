14341bd92ba372df0ed3cfd3ffa6e518f0a4d9a1
mly-zju <malingyang@foxmail.com>
2017-02-21 00:48:34 +0800

    regular update

---
+++ project/music-player/js/model.js
@@ -0,0 +1,86 @@
+var Model = function() {
+  this.data = {};
+  this.data.music = document.getElementById("music");
+  this.data.audioContext = new (window.AudioContext || window.webkitAudioContext)();
+  this.data.itemList = itemList;
+  this.data.itemCursor = itemList.length - 1;
+  this.data.itemLength = itemList.length;
+}
+
+Model.prototype.getCursor = function() {
+  return this.data.itemCursor;
+}
+
+Model.prototype.addCursor = function() {
+  this.data.itemCursor++;
+  if (this.data.itemCursor >= this.data.itemLength) {
+    this.data.itemCursor = 0;
+  }
+}
+
+Model.prototype.reduceCursor = function() {
+  this.data.itemCursor--;
+  if (this.data.itemCursor < 0) {
+    this.data.itemCursor = this.data.itemLength - 1;
+  }
+}
+
+Model.prototype.loadMusic = function() {
+  this.data.music.src = "http://mly-zju.github.io/project/music-player/music/" + this.data.itemCursor + ".mp3";
+  this.data.music.crossOrigin = 'anonymous';
+  // this.data.music.src = "./music/" + this.data.itemCursor + ".mp3";
+
+}
+
+Model.prototype.getTime = function() {
+  var current = this.data.music.currentTime;
+  var total = this.data.music.duration;
+  if (isNaN(current)) {
+    current = 0;
+  }
+  if (isNaN(total)) {
+    total = 0;
+  }
+  return {
+    currentTime: current,
+    totalTime: total
+  }
+}
+
+Model.prototype.setTime = function(current) {
+  this.data.music.currentTime = parseInt(current);
+}
+
+Model.prototype.getMusicTitle = function() {
+  return this.data.itemList[this.data.itemCursor].title;
+}
+
+Model.prototype.getIfPaused = function() {
+  return this.data.music.paused;
+}
+
+Model.prototype.playMusic = function() {
+  this.data.music.play();
+}
+
+Model.prototype.pauseMusic = function() {
+  this.data.music.pause();
+}
+
+Model.prototype.initAudio = function() {
+  this.data.source = this.data.audioContext.createMediaElementSource(this.data.music);
+  this.data.analyser = this.data.audioContext.createAnalyser();
+  this.data.source.connect(this.data.analyser);
+  this.data.analyser.connect(this.data.audioContext.destination);
+  this.data.output = new Uint8Array(this.data.analyser.frequencyBinCount);
+}
+
+Model.prototype.getFreq = function() {
+  this.data.analyser.getByteFrequencyData(this.data.output);
+  var data = [];
+  for (var i = 0; i < 1000;) {
+    data.push(this.data.output[i]);
+    i = i + 20;
+  }
+  return data;
+}
---
+++ project/music-player/js/view.js
@@ -0,0 +1,89 @@
+//音乐播放器部分
+var Music = function() {
+  this.cache = {};
+  this.dom = {};
+  this.dom.title = document.getElementById("music-title");
+  this.dom.wrapper = document.getElementById("music-bar");
+  this.dom.musicAll = document.getElementById("music-all");
+  this.dom.musicPlayed = document.getElementById("music-played");
+  this.dom.musicCtrl = document.getElementById("music-ctrl");
+  this.dom.musicTimeNow = document.getElementById("music-time-now");
+  this.dom.musicTimeTotal = document.getElementById("music-time-total");
+  this.dom.beginEnd = document.getElementById('music-button');
+  this.dom.next = document.getElementById('music-button-next');
+  this.dom.prev = document.getElementById('music-button-prev');
+}
+
+Music.prototype.init = function(title) {
+  this.dom.title.innerText = title;
+  this.dom.musicTimeNow.innerText = '00:00';
+  this.dom.musicTimeTotal.innerText = '00:00';
+}
+
+Music.prototype.update = function(currentTime, totalTime) {
+  this.dom.musicTimeNow.innerText = util.timeFormat(currentTime);
+  this.dom.musicTimeTotal.innerText = util.timeFormat(totalTime);
+  var percent = currentTime / totalTime * 100;
+  this.dom.musicPlayed.style.width = percent + "%";
+  this.dom.musicCtrl.style.left = percent - 1 + "%";
+}
+
+Music.prototype.begintoEnd = function() {
+  this.dom.beginEnd.style.backgroundImage = "url(./img/play.svg)";
+}
+
+Music.prototype.endtoBegin = function() {
+  this.dom.beginEnd.style.backgroundImage = "url(./img/pause.svg)";
+}
+
+//可视化图形部分
+var Visual = function() {
+  this.chart = echarts.init(document.getElementById('canvas'));
+  this.option = {
+    title: {},
+    tooltip: {
+      show: false
+    },
+    backgroundColor: 'rgba(255,255,255,0.5)',
+    grid: {
+      left: '1%',
+      right: '1%',
+      top: '1%',
+      bottom: '1%'
+    },
+    visualMap: {
+      type: 'continuous',
+      min: 0,
+      max: 255,
+      show: false
+    },
+    legend: {},
+    xAxis: {
+      data: [],
+      axisLine: {
+        show: false
+      }
+    },
+    yAxis: {
+      max: 255,
+      data: [],
+      axisLine: {
+        show: false
+      }
+    },
+    series: [{
+      name: '销量',
+      type: 'bar',
+      data: []
+    }]
+  }
+}
+
+Visual.prototype.init = function() {
+  this.chart.setOption(this.option);
+}
+
+Visual.prototype.update = function(data) {
+  this.option.series[0].data = data;
+  this.chart.setOption(this.option);
+}
