7a1a9b0788deebc5093648cc14d51493906157c1
mly-zju <malingyang@foxmail.com>
2017-02-21 16:59:08 +0800

    regular update

--- project/music-player/list.js
+++ project/music-player/list.js
@@ -9,5 +9,15 @@ var itemList = [
     title: '富士山下-陈奕迅'
   }, {
     title: '喜帖街-谢安琪'
+  }, {
+    title: '落花流水-陈奕迅'
+  }, {
+    title: 'k歌之王-陈奕迅'
+  }, {
+    title: '房间-刘瑞琦'
+  }, {
+    title: 'Bonfire-Knife Party'
+  }, {
+    title: "It's all over-Broken Family Band"
   }
 ];
